7c44ee657f548f17faf13fca168ed2b3a5accc7b
Ugur Kartal <kartalugur@gmail.com>
2024-06-24 09:18:21 +0200

    fix error on test

--- backend/src/test/java/de/filmempfehlung/backend/BackendApplicationTests.java
+++ backend/src/test/java/de/filmempfehlung/backend/BackendApplicationTests.java
@@ -2,8 +2,10 @@ package de.filmempfehlung.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {"spring.ai.openai.api-key=dummy-key"})
 class BackendApplicationTests {
 
     @Test
