e39298b83ea38adda44bf9156f546724466caf06
Ugur Kartal <kartalugur@gmail.com>
2024-06-26 11:42:21 +0200

    add get movie detail

--- backend/src/main/java/de/filmempfehlung/backend/services/abstracts/MovieService.java
+++ backend/src/main/java/de/filmempfehlung/backend/services/abstracts/MovieService.java
@@ -1,7 +1,9 @@
 package de.filmempfehlung.backend.services.abstracts;
 
 import de.filmempfehlung.backend.services.dtos.requests.OpenAiMovieRequest;
+import de.filmempfehlung.backend.services.dtos.responses.MovieCreatedResponse;
 
 public interface MovieService {
     String generateAndSaveMovieList(OpenAiMovieRequest openAiMovieRequest);
+    MovieCreatedResponse getMovieByMovieId(String id);
 }
