a53b9776ae1d7dc4ee88922601b28450dad1bbd9
Ugur Kartal <kartalugur@gmail.com>
2024-06-27 19:19:03 +0200

    add docs on controller

--- backend/src/main/java/de/filmempfehlung/backend/controllers/MovieController.java
+++ backend/src/main/java/de/filmempfehlung/backend/controllers/MovieController.java
@@ -7,18 +7,40 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * This is a REST controller for managing movies.
+ * It uses Spring's @RestController annotation, so it's assumed that all methods return @ResponseBody by default.
+ * The @RequestMapping("/api/movies") annotation is used to map web requests onto specific handler methods.
+ * It is a required arguments constructor, meaning that it requires an argument of type MovieService to be instantiated.
+ */
 @RestController
 @RequestMapping("/api/movies")
 @RequiredArgsConstructor
 public class MovieController {
+    // The MovieService is automatically injected by Spring due to the @RequiredArgsConstructor annotation from Lombok.
     private final MovieService movieService;
 
+    /**
+     * This method is used to generate and save a movie list.
+     * It is mapped to the POST HTTP method and the "/generate" URL path.
+     * The @ResponseStatus annotation is used to define the HTTP status code that should be returned.
+     * The OpenAiMovieRequest object is automatically populated from the JSON in the request body due to the @RequestBody annotation.
+     * @param openAiMovieRequest the request body containing the parameters for generating the movie list
+     * @return a String indicating the result of the operation
+     */
     @PostMapping("/generate")
     @ResponseStatus(HttpStatus.CREATED)
     public String generateAndSaveMovieList(@RequestBody OpenAiMovieRequest openAiMovieRequest) {
         return movieService.generateAndSaveMovieList(openAiMovieRequest);
     }
 
+    /**
+     * This method is used to get a movie by its ID.
+     * It is mapped to the GET HTTP method and the "/{id}" URL path, where {id} is a path variable.
+     * The @PathVariable annotation is used to bind the method parameter to the path variable.
+     * @param id the ID of the movie to retrieve
+     * @return a MovieCreatedResponse object containing the details of the requested movie
+     */
     @GetMapping("/{id}")
     public MovieCreatedResponse getMovieByMovieId(@PathVariable String id) {
         return movieService.getMovieByMovieId(id);
